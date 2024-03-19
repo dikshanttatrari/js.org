af90b8c7467bbe48367f74d533c66e3c46a8f71c
Dikshant Tatrari <82396523+dikshanttatrari@users.noreply.github.com>
2024-03-19 19:32:12 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -38,6 +38,7 @@
  */
 
 var cnames_active = {
+  "sunrise-public-school": "https://dikshanttatrari.github.io/sunrise-pubic-school"
   "": "js-org.github.io",
   "01mz": "01mz.github.io",
   "100dayz": "deadcoder0904.github.io/100dayz",
